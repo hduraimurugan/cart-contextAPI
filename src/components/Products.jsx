5e25e32426af9a5a5958162d4813773f7cb69cea
Duraimurugan-H <hduraimurugan@gmail.com>
2024-08-20 23:46:26 +0530

    ui changes

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -57,7 +57,7 @@ export default function Products() {
     notifyRemovedFromCart(product);
   };
 
-  const filteredProducts = products.filter(product => 
+  const filteredProducts = products.filter(product =>
     product.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
@@ -65,45 +65,53 @@ export default function Products() {
     <div className='flex flex-col justify-center bg-gray-200'>
       <ToastContainer />
 
-      <div className='flex justify-center gap-10 items-center px-20 py-5'>
-        <h1 className='text-2xl font-row-font  mt-10 text-center mb-10'>Ship-Kart</h1>
-        
-        <input
-          type="text"
-          placeholder="Search Products Here"
-          className="px-4 py-4 md:block hidden w-1/2 shadow-md text-black text-md rounded-lg focus:outline-none"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        
+      <div className='flex flex-col md:flex-row justify-around gap-4 md:gap-10 items-center px-20 py-5'>
+        <div className='flex flex-row gap-7'>
+          <div>
+          <h1 className='md:text-2xl text-xl font-row-font mt-10 text-center mb-10'>Ship-Kart</h1>
+          </div>
+          <div className='flex items-center'>
+            <input
+              type="text"
+              placeholder="Search Products Here"
+              className="px-4 py-2 shadow-md text-black text-md rounded-lg focus:outline-none"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </div>
+
+        </div>
+
+        <div>
         {!showModal && (
           <button
-            className='px-4 py-2 flex flex-row shadow-md bg-yellow-400 text-black text-xs font-bold uppercase rounded hover:bg-yellow-500 focus:outline-none focus:bg-yellow-600'
+            className='mt-4 md:mt-0 px-4 py-2 flex flex-row shadow-md bg-yellow-400 text-black text-xs font-bold uppercase rounded hover:bg-yellow-500 focus:outline-none focus:bg-yellow-600'
             onClick={toggle}
           >
             <BsCart4 size={16} /> Cart ({cartItems.length})
           </button>
-        )}
+        )}</div>
+        
       </div>
 
       <div className='bg-blue-950 text-white'>
-        <h1 className='flex md:flex-row flex-col items-center md:justify-center text-4xl font-semibold font-list-font px-10 py-20 text-center'>Welcome  to  Our  Upgraded  Ship-Cart  <BsCart4 size={36} /></h1>
+        <h1 className='flex md:flex-row flex-col items-center md:justify-center md:text-4xl text-md font-semibold font-list-font px-10 md:py-20 py-10 text-center'>Welcome to Our Upgraded Ship-Cart <BsCart4 size={36} /></h1>
       </div>
 
       <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-10 px-10'>
         {filteredProducts.map(product => (
           <div key={product.id} className='bg-white shadow-md rounded-lg flex flex-col justify-between px-10 py-10'>
-            
+
             <div>
-            <img src={product.image} alt={product.title} className='rounded-md h-48' />
+              <img src={product.image} alt={product.title} className='rounded-md h-48' />
             </div>
-            
+
             <div className='mt-4'>
               <h1 className='text-lg uppercase font-bold'>{product.title}</h1>
               <p className='mt-2 text-gray-600 text-sm'>{product.description.slice(0, 40)}...</p>
               <p className='mt-2 text-gray-600'>Rs.{product.price}/-</p>
             </div>
-            
+
             <div className='mt-6 flex justify-between items-center'>
               {!cartItems.find(item => item.id === product.id) ? (
                 <button
@@ -145,7 +153,7 @@ export default function Products() {
           </div>
         ))}
       </div>
-      
+
       <Cart showModal={showModal} toggle={toggle} />
     </div>
   );
